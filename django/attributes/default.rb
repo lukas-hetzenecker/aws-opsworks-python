973e7fbc14eaee6867b90fa684b07dd90b1942a1
Lukas Hetzenecker <lukas.hetzenecker@gmail.com>
2013-12-05 02:12:03 +0100

    update, add application_python

--- django/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['python']['version'] = '27'
-default['python']['distribute_script_url'] = 'http://python-distribute.org/distribute_setup.py'
--- django/recipes/setup.rb
+++ django/recipes/setup.rb
@@ -1,2 +1,13 @@
-include_recipe 'python::source'
+node[:deploy].each do |application, deploy|
+ if deploy[:application_type] != 'python'
+   Chef::Log.debug("Skipping setup::python application #{application} as it is not a python app")
+   next
+ end
+
+ Chef::Log.debug("Executing setup::python application #{application} is a python app")
+
+ include_recipe 'python::default'
+ Chef::Log.debug("Installing -> python #{node['python']['install_method']}")
+
+end
 
